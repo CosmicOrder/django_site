179cd5fc4063f07f86dcdc8a54295a2f34ce0ce5
Igor Zhukov <Matrixxturn@yandex.ru>
2022-05-22 11:45:53 +0300

    fix: add titles

--- my_site/views.py
+++ my_site/views.py
@@ -2,20 +2,33 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'my_site/index.html')
+    return render(request, 'my_site/index.html',
+                  {'title': 'Конструкторское бюро'})
 
 
 def reverse_engineering(request):
     return render(request, 'my_site/reverse-engineering.html',
-                  {'page_name': '-reverse-engineering'})
+                  {
+                      'page_name': '-reverse-engineering',
+                      'title': 'Реверсивный инжениринг'
+                  })
 
 
 def drafts(request):
-    return render(request, 'my_site/drafts.html', {'page_name': '-drafts'})
+    return render(request, 'my_site/drafts.html',
+                  {
+                      'page_name': '-drafts',
+                      'title': 'Разработка КД'
+
+                  })
 
 
 def visualize(request):
-    return render(request, 'my_site/visualize.html', {'page_name': '-visualize'})
+    return render(request, 'my_site/visualize.html',
+                  {
+                      'page_name': '-visualize',
+                      'title': '3д моделирование'
+                  })
 
 
 def projects(request):
