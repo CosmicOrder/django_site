99f622c38ac18a29d4a4a769952a7a3edcfa63e1
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-07 00:13:21 +0300

    fix: add dj-database-url for DB settings and several samples for site

--- reverse_engineering_site/settings.py
+++ reverse_engineering_site/settings.py
@@ -12,24 +12,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+import dj_database_url
+from environs import Env
 
-load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+env = Env()
+env.read_env()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+SECRET_KEY = env.str('SECRET_KEY', '...')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = env.bool('DEBUG', False)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = ['localhost', 'top-reverse.ru']
-
-# Application definition
+DATABASES = {'default': dj_database_url.config()}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -70,20 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'reverse_engineering_site.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
