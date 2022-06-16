65db269f7db713e88ab197742845c1e329b31899
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-17 00:10:10 +0300

    fix: use env.list in ALLOWED_HOSTS

--- reverse_engineering_site/settings.py
+++ reverse_engineering_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env.str('SECRET_KEY', '...')
 
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 DATABASES = {'default': dj_database_url.config()}
 
